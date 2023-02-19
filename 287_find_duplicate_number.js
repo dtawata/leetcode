ca1961bd0b5f1b71bc4dafbe2ed08cf9bab283b9
Daniel Tawata <danieltawata@Daniels-MacBook-Pro.local>
2023-02-19 13:46:19 -0800

    finished 287

---
+++ 287_find_duplicate_number.js
@@ -0,0 +1,22 @@
+const findDuplicate = (nums) => {
+  let slow = 0;
+  let fast = 0;
+  while (true) {
+    slow = nums[slow];
+    const step = nums[fast];
+    fast = nums[step];
+    if (slow === fast) {
+      slow = 0;
+      break;
+    }
+  }
+
+  while (true) {
+    slow = nums[slow];
+    fast = nums[fast];
+    if (slow === fast) return slow;
+  }
+};
+
+const nums = [1,3,4,2,2]; // 2
+console.log(findDuplicate(nums));
