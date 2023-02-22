5e862019ea2e9195c67a9d1c691b050f2dd3d622
Daniel Tawata <danieltawata@Daniels-MacBook-Pro.local>
2023-02-22 13:20:33 -0800

    finished 141

---
+++ 141_linked_list_cycle.js
@@ -0,0 +1,13 @@
+const hasCycle = (head) => {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === true) return true;
+  }
+  return false;
+};
+
+head = [3,2,0,-4], pos = 1 // true
+console.log(hasCycle(head));
