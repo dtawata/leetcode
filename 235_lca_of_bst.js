52ca9c4fd08ec604f14abbdd743e44bf2b8f74e2
Daniel <tawata.daniel@gmail.com>
2023-02-25 13:41:27 -0800

    finished 235

---
+++ 235_lca_of_bst.js
@@ -0,0 +1,12 @@
+const lowestCommonAncestor = (root, p, q) => {
+  if (root.val > p.val && root.val > q.val) {
+    return lowestCommonAncestor(root.left, p, q);
+  }
+
+  if (root.val < p.val && root.val < q.val) {
+    return lowestCommonAncestor(root.right, p, q);
+  }
+
+  return root;
+};
+
