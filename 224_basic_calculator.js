107f619b4183f7e7867d36694bb09c029a9dcd16
Daniel <tawata.daniel@gmail.com>
2023-03-12 00:51:56 -0800

    finished 227 (optimized)

--- 224_basic_calculator.js
+++ 224_basic_calculator.js
@@ -1,73 +1,23 @@
-// const add = (equation) => {
-//   equation = equation.join('').split('');
-//   let total = 0;
-//   let num = '0';
-//   let symbol = '+';
-//   for (let i = 0; i < equation.length; i++) {
-//     const char = equation[i];
-//     if (char === ' ') continue;
-//     else if (Number(char) || Number(char) === 0) num = num + char;
-//     else {
-//       if (symbol === '-' && equation[i - 1] === '-') {
-//         symbol = '+';
-//         continue;
-//       }
-//       if (symbol === '+') total += Number(num);
-//       else if (symbol === '-') total -= Number(num);
-//       else if (symbol === '*') total *= Number(num);
-//       else if (symbol === '/') total /= Number(num);
-//       symbol = char;
-//       num = '0';
-//     }
-//   }
-//   if (symbol === '+') total += Number(num);
-//   else if (symbol === '-') total -= Number(num);
-//   else if (symbol === '*') total *= Number(num);
-//   else if (symbol === '/') total /= Number(num);
-//   return total;
-// };
-
-// const calculate = (s) => {
-//   s = s.replace(/ /g,'').split('');
-//   const start = [];
-//   let count = 0;
-//   for (let i = 0; i < s.length; i++) {
-//     const char = s[i];
-//     if (char === '(') {
-//       start.push(i);
-//     }
-//     else if (char === ')') {
-//       const open = start.pop();
-//       const end = i;
-//       const temp = add(s.slice(open + 1, end));
-//       s.splice(open, end - open + 1, temp);
-//       i -= end - open;
-//     }
-//   }
-//   return add(s);
-// };
-
 const calculate = (s, start = 0) => {
   let total = 0;
   let curr = '0';
-  let sign = '+';
+  let plus = true;
 
   for (let i = start; i < s.length; i++) {
     const char = s[i];
     if (char === ' ') continue;
-    if (char === ')') return sign === '+' ? [total + Number(curr), i] : [total - Number(curr), i];
-    if (Number(char) || Number(char) === 0) {
+    if (char === ')') return plus ? [total + Number(curr), i] : [total - Number(curr), i];
+    if (!isNaN(char)) {
       curr = curr + char;
     } else if (char === '(') {
       [curr, i] = calculate(s, i + 1);
     } else {
-      if (sign === '+') total += Number(curr);
-      else if (sign === '-') total -= Number(curr);
-      sign = char;
+      total = plus ? total + Number(curr) : total - Number(curr);
+      plus = char === '+';
       curr = '0';
     }
   }
-  return sign === '+' ? total + Number(curr) : total - Number(curr);
+  return plus ? total + Number(curr) : total - Number(curr);
 };
 
 s = "(1+(4+5+2)-3)+(6+8)" // 23
--- 227_basic_calculator_2.js
+++ 227_basic_calculator_2.js
@@ -1,77 +1,32 @@
-const breakdown = (s) => {
-  let equation = '';
-  let prefix = '';
-  let suffix = '';
-  let symbol = '';
-  for (const char of s) {
-    if (char === ' ') continue;
-    if (Number(char) || char === '0') {
-      if (!symbol) prefix = prefix + char;
-      else suffix = suffix + char;
-    } else {
-      if (!symbol && (char === '+' || char === '-')) {
-        equation = equation + prefix + char;
-        prefix = '';
-      } else if (!symbol && (char === '*' || char === '/')) {
-        symbol = char;
-      } else if (symbol && char === '+') {
-        if (symbol === '*') {
-          equation = equation + (Number(prefix) * Number(suffix)) + char;
-        } else {
-          equation = equation + Math.floor(Number(prefix) / Number(suffix)) + char;
-        }
-        prefix = '';
-        suffix = '';
-        symbol = '';
-      } else if (symbol && char === '-') {
-        if (suffix) {
-          if (symbol === '*') {
-            equation = equation + (Number(prefix) * Number(suffix)) + char;
-          } else {
-            equation = equation + Math.floor(Number(prefix) / Number(suffix)) + char;
-          }
-          prefix = '';
-          suffix = '';
-          symbol = '';
-        } else {
-          suffix = char;
-        }
-      } else if (symbol && (char === '*' || char === '/')) {
-        if (symbol === '*') {
-          prefix = Number(prefix) * Number(suffix);
-          symbol = char;
-          suffix = '';
-        } else {
-          prefix = Math.floor(Number(prefix) / Number(suffix));
-          symbol = char;
-          suffix = '';
-        }
-      }
-    }
-  }
-  if (symbol === '*') prefix = Number(prefix) * Number(suffix);
-  else if (symbol === '/') prefix = Math.floor(Number(prefix) / Number(suffix));
-  return equation + prefix;
-};
-
-const calculate = (s) => {
+const calculate = (s, start = 0) => {
   let total = 0;
-  s = breakdown(s);
   let curr = '0';
-  let lastSymbol = '+';
-  for (const char of s) {
-    if (Number(char) || char === '0' || char === '.') {
-      curr += char;
+  let sign = '+';
+
+  for (let i = start; i < s.length; i++) {
+    const char = s[i];
+    if (char === ' ') continue;
+    if (!isNaN(char)) {
+      curr = curr + char;
     } else {
-      if (lastSymbol === '+') {
-        total += Number(curr);
-      } else {
-        total -= Number(curr);
+      if (char === '+' || char === '-') {
+        if (sign === '+') total += Number(curr);
+        else if (sign === '-') total -= Number(curr);
+        else if (sign === '*') total *= Number(curr);
+        else if (sign === '/') total /= Number(curr);
+        sign = char;
+        curr = '0';
+      } else if (char === '*' || char === '/') {
+        let temp = '0';
+        i++;
+        while (!isNaN(s[i])) {
+          if (s[i] !== ' ') temp = temp + s[i];
+          i++;
+        }
+        curr = char === '*' ? Number(curr) * Number(temp) : Math.floor(Number(curr) / Number(temp));
+        i--;
       }
-      lastSymbol = char;
-      curr = '0';
     }
   }
-  if (lastSymbol === '+') return Math.floor(total + Number(curr));
-  else return Math.floor(total - Number(curr));
+  return sign === '+' ? total + Number(curr) : total - Number(curr);
 };
